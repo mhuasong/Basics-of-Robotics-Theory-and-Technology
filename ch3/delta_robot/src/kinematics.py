f9040b78ed4cb2e2d573a406e97795d4bcc76008
Min Huasong <44629028+mhuasong@users.noreply.github.com>
2023-09-26 11:33:56 +0800

    Update kinematics.py

--- ch3/delta_robot/src/kinematics.py
+++ ch3/delta_robot/src/kinematics.py
@@ -1,30 +1,99 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import numpy as np
+import math
+from math import pow, sqrt, pi, atan, sin, cos
 import sys
-import numpy
-from math import sin, cos, sqrt, atan, pi
-import rospy
-from sensor_msgs.msg import JointState
-from std_msgs.msg import Header
 
-joint_pub = None
+""" ---------- 3.11.2 Delta机器人运动学分析 ---------- """
+## 表3-8 Delta机器人几何参数表
+L = 0.300 # 驱动臂等效臂长
+l = 0.800 # 从动臂等效臂长
+S_p = 0.100 # 动平台等边三角形的边长
+wb = 0.180 # 静坐标系{B}的原点到等腰三角形边的垂直直线距离
+wp = 0.030 # 动坐标系{P}的原点到等腰三角形边的垂直直线距离
+up = 0.035 # 动坐标系{P}的原点到等腰三角形顶点的直线距离
 
-""" ----------表3-8 Delta机器人几何参数表---------- """
-sb = 0.567  #静平台等边三角形的边长mm
-sp = 0.076  #动平台等边三角形的边长mm
-L = 0.524  #驱动臂等效臂长
-l = 1.244  #从动臂等效臂长（平行四边形长度）
-h = 0.131
-wb = (sqrt(3)/6) *sb  #静坐标系{B}的原点到等腰三角形边的垂直直线距离
-ub = (sqrt(3)/3) *sb  #静坐标系{B}的原点到等腰三角形顶点的直线距离
-wp = (sqrt(3)/6) *sp  #动坐标系{P}的原点到等腰三角形边的垂直直线距离
-up = (sqrt(3)/3) *sp  #动坐标系{P}的原点到等腰三角形顶点的直线距离
-dtr = pi / 180.0  #degree to rad
-rtd = 180.0 / pi  # rad to degree
+## 2)Delta机器人运动学逆解
+def calculate_delta_robot_angle(P):
+    x, y, z = P
 
+    # 计算Delta机器人的三个运动学约束方程中的a, b, c 公式(3-114,3-114,3-116)
+    a = wb - up
+    b = (S_p / 2.0) - ((sqrt(3.0) / 2.0) * wb)
+    c = wp - (wb / 2.0)
 
-""" ----------Delta Robot Kinematics Analysis---------- """
-## Forward Kinematics(FK) of Delta robot
+    # reserve storage and define size/shape for the most used variables
+    # 为最常用的变量预留存储空间并定义大小/形状
+    E = np.zeros(3)  # Variable for calculate angle
+    F = np.zeros(3)  # Variable for calculate angle
+    G = np.zeros(3)  # Variable for calculate angle
+    # 3.11.2 2)Delta机器人运动学逆解
+    # Calculate E, F, G
+    E[0] = 2.0 * L * (y + a)
+    F[0] = 2.0 * z * L
+    G[0] = pow(x, 2) + pow(y, 2) + pow(z, 2) + pow(a, 2) + \
+           pow(L, 2) + (2.0 * y * a) - pow(l, 2)
+    E[1] = -L * ((sqrt(3.0) * (x + b)) + y + c)
+    F[1] = 2.0 * z * L
+    G[1] = pow(x, 2) + pow(y, 2) + pow(z, 2) + pow(b, 2) + pow(c, 2) + \
+           pow(L, 2) + (2.0 * ((x * b) + (y * c))) - pow(l, 2)
+    E[2] = L * ((sqrt(3.0) * (x - b)) - y - c)
+    F[2] = 2.0 * z * L
+    G[2] = pow(x, 2) + pow(y, 2) + pow(z, 2) + pow(b, 2) + pow(c, 2) + \
+           pow(L, 2) + (2.0 * (-(x * b) + (y * c))) - pow(l, 2)
+
+    # Calculate angle in rad and degrees
+    angle_upper_arm_rad = np.zeros(3)  # solution angle of motors in rad
+    angle_upper_arm_deg = np.zeros(3)
+    for i in range(0, 3):
+        # 对t_i的一元二次方程求根
+        t1 = (-F[i] + sqrt(pow(F[i],2) - pow(G[i],2) + pow(E[i],2))) / (G[i] - E[i])
+        t2 = (-F[i] - sqrt(pow(F[i],2) - pow(G[i],2) + pow(E[i],2))) / (G[i] - E[i])
+        # t_i = tan(θ_i/2) 公式(3-117)
+        sol1 = 2 * atan(t1)
+        sol2 = 2 * atan(t2)
+        # 一般选择支链向外弯曲的姿态
+        if ((sol1 < pi / 2) & (sol1 > -pi / 2)):
+            angle_upper_arm_rad[i] = sol1
+        elif ((sol2 < pi / 2) & (sol2 > -pi / 2)):
+            angle_upper_arm_rad[i] = sol2
+        angle_upper_arm_deg[i] = math.degrees(angle_upper_arm_rad[i])
+    return angle_upper_arm_rad, angle_upper_arm_deg
+def calculate_angle_lower_arms(P, angle_upper_arm_rad):
+    # 计算角度
+    p0 = np.array(P)
+    p1 = np.array([0, -up, 0])
+    p2 = np.array([S_p/2, wp, 0])
+    p3 = np.array([-S_p/2, wp, 0])
+    a1 = np.array([0, -wb-L*cos(angle_upper_arm_rad[0]), - L*sin(angle_upper_arm_rad[0])])
+    a2 = np.array([((sqrt(3))/2)*(wb+(L*cos(angle_upper_arm_rad[1]))),
+                   0.5*(wb+(L*cos(angle_upper_arm_rad[1]))), - L*sin(angle_upper_arm_rad[1])])
+    a3 = np.array([-((sqrt(3))/2)*(wb+(L*cos(angle_upper_arm_rad[2]))),
+                   0.5*(wb+(L*cos(angle_upper_arm_rad[2]))), - L*sin(angle_upper_arm_rad[2])])
+    pf1 = p0 + p2
+    pf2 = p0 + p3
+    pf3 = p0 + p1
+
+    d1_square = ((pf1 - a1)*(pf1 - a1)).sum()
+    d2_square = ((pf2 - a2)*(pf2 - a2)).sum()
+    d3_square = ((pf3 - a3)*(pf3 - a3)).sum()
+    D1 = math.acos((d1_square - pow(S_p,2)-pow(l,2))/((-2)*S_p*l))
+    alpha1 = pi - D1
+    beta1 = pi - angle_upper_arm_rad[0]
+    final_angle_lower_arm1 = beta1 - alpha1 - (pi/2) + 0.15
+    D2 = math.acos((d2_square - pow(S_p,2)-pow(l,2))/((-2)*S_p*l))
+    alpha2 = pi - D2
+    beta2 = pi - angle_upper_arm_rad[1]
+    final_angle_lower_arm2 = beta2 - alpha2 - (pi/2)
+    D3 = math.acos((d3_square - pow(S_p,2)-pow(l,2))/((-2)*S_p*l))
+    alpha3 = pi - D3
+    beta3 = pi - angle_upper_arm_rad[2]
+    final_angle_lower_arm3 = beta3 - alpha3 - (pi/2) + 0.1
+    end_effector = (angle_upper_arm_rad[0] + final_angle_lower_arm1) * -1
+    return final_angle_lower_arm1, final_angle_lower_arm2, final_angle_lower_arm3, end_effector
+
+## 3)Delta机器人运动学正解
 def forward(theta1, theta2, theta3):
     ## 三个虚拟的球体中心B_A_iv
     # B_A_1v
@@ -33,12 +102,12 @@ def forward(theta1, theta2, theta3):
     z1 = -L * sin(theta1)
 
     # B_A_2v
-    x2 = sqrt(3)* (wb + L * cos(theta2))/2 - sp / 2
+    x2 = sqrt(3)* (wb + L * cos(theta2))/2 - S_p / 2
     y2 = (wb + L * cos(theta2)) / 2 - wp
     z2 = -L * sin(theta2)
 
     # B_A_3v
-    x3 = -sqrt(3) * (wb + L * cos(theta3)) / 2 + sp / 2
+    x3 = -sqrt(3) * (wb + L * cos(theta3)) / 2 + S_p / 2
     y3 = (wb + L * cos(theta3)) / 2 - wp
     z3 = -L * sin(theta3)
 
@@ -58,12 +127,10 @@ def forward(theta1, theta2, theta3):
     b2 = l2**2-l3**2-x2**2-y2**2-z2**2+x3**2+y3**2+z3**2
 
     if a13 == 0 or a23 == 0 or (a11 / a13 - a21 / a23)==0:
-        print("a13=0或a23=0")
-        # 使用高度相等的z高度的球体相交算法
-        # (3-120)
+        # 使用高度相等的z高度的球体相交算法 公式(3-120)
         x = (a22 * b1 - a12 * b2) / (a11 * a22 - a12 * a21)
         y = (a11 * b2 - a21 * b1) / (a11 * a22 - a12 * a21)
-        # (3-121)
+        # 公式(3-121)
         zn = z1
         z1 = (2 * zn + sqrt(4 * l1 * l1 - 4 * (x - x1) * (x - x1) - 4 * (y - y1) * (y - y1))) / 2
         z2 = (2 * zn - sqrt(4 * l1 * l1 - 4 * (x - x1) * (x - x1) - 4 * (y - y1) * (y - y1))) / 2
@@ -77,9 +144,6 @@ def forward(theta1, theta2, theta3):
         # 使用非相等的z高度的球体相交算法
         # x=f(y)
         a1 = a11 / a13 - a21 / a23
-        #if a1 == 0:
-            #print("a1=0")
-            #return None
         a2 = a12 / a13 - a22 / a23
         a3 = b2 / a23 - b1 / a13
         a4 = -a2 / a1
@@ -109,137 +173,3 @@ def forward(theta1, theta2, theta3):
             return [x1, y1, z1]
         elif z2 < 0:
             return [x2, y2, z2]
-
-## Inverse Kinematics(IK) of Delta robot
-def inverse(P):
-    x, y, z = P
-
-    a = wb - up
-    b = sp / 2 - (sqrt(3) / 2) * wb
-    c = wp - wb / 2
-
-    e1 = 2 * L * (y + a)
-    f1 = 2 * z * L
-    g1 = x ** 2 + y ** 2 + z ** 2 + a ** 2 + L ** 2 + 2 * y * a - l ** 2
-    e2 = -L * (sqrt(3) * (x + b) + y + c)
-    f2 = 2 * z * L
-    g2 = x ** 2 + y ** 2 + z ** 2 + b ** 2 + c ** 2 + L ** 2 + 2 * x * b + 2 * y * c - l ** 2
-    e3 = L * (sqrt(3) * (x - b) - y - c)
-    f3 = 2 * z * L
-    g3 = x ** 2 + y ** 2 + z ** 2 + b ** 2 + c ** 2 + L ** 2 - 2 * x * b + 2 * y * c - l ** 2
-
-    sol1max = 2 * atan((-f1 + sqrt((e1 * e1) + (f1 * f1) - (g1 * g1))) / (g1 - e1))
-    sol1min = 2 * atan((-f1 - sqrt((e1 * e1) + (f1 * f1) - (g1 * g1))) / (g1 - e1))
-    sol2max = 2 * atan((-f2 + sqrt((e2 * e2) + (f2 * f2) - (g2 * g2))) / (g2 - e2))
-    sol2min = 2 * atan((-f2 - sqrt((e2 * e2) + (f2 * f2) - (g2 * g2))) / (g2 - e2))
-    sol3max = 2 * atan((-f3 + sqrt((e3 * e3) + (f3 * f3) - (g3 * g3))) / (g3 - e3))
-    sol3min = 2 * atan((-f3 - sqrt((e3 * e3) + (f3 * f3) - (g3 * g3))) / (g3 - e3))
-
-    thetas = [0, 0, 0]
-    if ((sol1min < pi / 2) & (sol1min > -pi / 2)):
-        thetas[0] = sol1min
-    elif ((sol1max < pi / 2) & (sol1max > -pi / 2)):
-        thetas[0] = sol1max
-
-    if ((sol2min < pi / 2) & (sol2min > -pi / 2)):
-        thetas[1] = sol2min
-    elif ((sol2max < pi / 2) & (sol2max > -pi / 2)):
-        thetas[1] = sol2max
-
-    if ((sol3min < pi / 2) & (sol3min > -pi / 2)):
-        thetas[2] = sol3min
-    elif ((sol3max < pi / 2) & (sol3max > -pi / 2)):
-        thetas[2] = sol3max
-
-    return thetas
-
-
-""" ----------Delta Robot Kinematics Examples---------- """
-## Inverse Kinematics Examples
-def IK():
-    """
-    B_P_P={0 0 -0.9}(m),计算出的IPK结果为θ={-20.5° -20.5° -20.5°}
-    B_P_P={0.3 0.5 -1.1}(m),计算出的IPK结果为θ={47.5° -11.6° 21.4°}
-    """
-    # Nominal Position
-    B_P_P = [0, 0, -0.9]
-    # General Position
-    B_P_P = [0.3, 0.5, -1.1]
-    thetas = inverse(B_P_P)
-    print("θ =", thetas, "(rad)")
-    thetas[0] /= dtr
-    thetas[1] /= dtr
-    thetas[2] /= dtr
-    print("θ =", thetas, "(degree)")
-#IK()
-
-## Forward Kinematics Examples
-def FK():
-    """
-    θ={0 0 0}(rad),（使用高度相等的z高度的球体相交算法）,计算出的FPK结果为B_P_P={0 0 -1.065}(m)
-    θ={10° 20° 30°}(degree),（使用非相等的z高度的球体相交算法）,计算出的FPK结果为B_P_P={0.108 -0.180 -1.244}(m)
-    """
-    # Nominal Theta
-    thetas = [0, 0, 0]
-    # General Theta
-    thetas = [10 * dtr, 20 * dtr, 30 * dtr]
-    B_P_P = forward(thetas[0], thetas[1], thetas[2])
-    print("B_P_P =", B_P_P, "(m)")
-#FK()
-
-def movej(j1,j2,j3):
-    print "move joint"
-    joint_pub = rospy.Publisher('joint_states', JointState, queue_size = 10)
-    rate = rospy.Rate(1)
-    joints_configuration = JointState()
-    joints_configuration.header = Header()
-    joints_configuration.header.stamp = rospy.Time.now()
-    joints_configuration.name = ['link_0_JOINT_1', 'link_0_JOINT_2', 'link_0_JOINT_3']
-    joints_configuration.position = [j1 * dtr, j2* dtr, j3* dtr]
-    joints_configuration.velocity = []
-    joints_configuration.effort = []
-    while not rospy.is_shutdown():
-       joints_configuration.header.stamp = rospy.Time.now()
-       rospy.loginfo(joints_configuration)
-       break
-    joint_pub.publish(joints_configuration)
-#movej()
-
-def kinematics_test():
-    """
-    循环检查示例
-    """
-    B_P_P = [0, 0, -0.9]
-    B_P_P = [0.3, 0.5, -1.1]
-    print "set B_P_P =", B_P_P, "(m)"
-    thetas = inverse(B_P_P)
-    print "inverse kinematics thetas =", thetas, "(rad)"
-    B_P_P = forward(thetas[0], thetas[1], thetas[2])
-    print "forward kinematics:B_P_P =", B_P_P, "(m)"
-    
-    thetas = [0, 0, 0]
-    thetas = [10 * dtr, 20 * dtr, 30 * dtr]
-    print "set thetas =", thetas, "(rad)" 
-    B_P_P = forward(thetas[0], thetas[1], thetas[2])
-    print "forward kinematics B_P_P =", B_P_P, "(m)" 
-    thetas = inverse(B_P_P)
-    print "inverse kinematics thetas =", thetas,  "(rad)"
-    
-def main():
-    
-    # Init ROS
-    rospy.init_node('delta_joint_publisher', anonymous=True)
-    # Subscribers
-    #rospy.Subscriber('joint_states', JointState, movej)
-    # Publishers
-    joint_pub = rospy.Publisher('joint_states', JointState, queue_size = 10)
-
-    movej(10,20,30)
-    rospy.sleep(2)
-
-if __name__ == '__main__':
-    try:
-        main()
-    except rospy.ROSInterruptException:
-        print ("Program interrupted before completion")
-
