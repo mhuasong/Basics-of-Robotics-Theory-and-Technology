bf45c27155d191a1b07e3ad2d01af93c1de3422a
Min Huasong <44629028+mhuasong@users.noreply.github.com>
2024-01-12 10:42:22 +0800

    Update simple_cmd.py

--- ch3/omni_robot/src/simple_cmd.py
+++ ch3/omni_robot/src/simple_cmd.py
@@ -2,57 +2,98 @@
 
 import rospy
 from std_msgs.msg import Float64
+import math
+from math import sqrt
 
 
 if __name__=="__main__":
 
     rospy.init_node('Simple_Vel_Cmd')
-    pub0 = rospy.Publisher('/omni_robot/back_joint_velocity_controller/command', Float64, queue_size=1)
-    pub1 = rospy.Publisher('/omni_robot/left_joint_velocity_controller/command', Float64, queue_size=1)
-    pub2 = rospy.Publisher('/omni_robot/right_joint_velocity_controller/command', Float64, queue_size=1)
+    back_pub = rospy.Publisher('/omni_robot/back_joint_velocity_controller/command', Float64, queue_size=1)
+    left_pub = rospy.Publisher('/omni_robot/left_joint_velocity_controller/command', Float64, queue_size=1)
+    right_pub = rospy.Publisher('/omni_robot/right_joint_velocity_controller/command', Float64, queue_size=1)
 
     rate=rospy.Rate(2)
 
-    vel0 = Float64()
-    vel1 = Float64()
-    vel2 = Float64()
+    back_vel = Float64()
+    left_vel = Float64()
+    right_vel = Float64()
 
-    vel0.data = 0
-    vel1.data = 0
-    vel2.data = 0
+    back_vel.data = 0
+    left_vel.data = 0
+    right_vel.data = 0
 
     while not rospy.is_shutdown():
 	key = raw_input()
-    	## forward line
-	if key == 'f':
-	   vel0 = 0
-	   vel1 = -10
-	   vel2 = 10
-    	## left line    
-	elif key == 'g':
-	   vel0 = 10
-	   vel1 = -10
-	   vel2 = -10
+    	## 1 forward line
+	if key == 'w':
+	   back_vel = 0
+	   left_vel = -5*sqrt(3)
+	   right_vel = 5*sqrt(3)
+
+    	## 5 back line
+	elif key == 's':
+	   back_vel = 0
+	   left_vel = 5*sqrt(3)
+	   right_vel = -5*sqrt(3)
+
+    	## 3 left line    
+	elif key == 'a':
+	   back_vel = -10
+	   left_vel = 5
+	   right_vel = 5
+
+    	## 7 right line    
+	elif key == 'd':
+	   back_vel = 10
+	   left_vel = -5
+	   right_vel = -5
+
+    	## 2 left forward 
+	elif key == 'q':
+	   back_vel = -5*sqrt(2)
+	   left_vel = 5*(sqrt(2)-sqrt(6))/2
+	   right_vel = 5*(sqrt(2)+sqrt(6))/2
+
+    	## 8 right forward
+	elif key == 'e':
+	   back_vel = 5*sqrt(2)
+	   left_vel = -5*(sqrt(2)+sqrt(6))/2
+	   right_vel = -5*(sqrt(2)-sqrt(6))/2
+
+    	## 4 left back
+	elif key == 'z':
+	   back_vel = -5*sqrt(2)
+	   left_vel = 5*(sqrt(2)+sqrt(6))/2
+	   right_vel = 5*(sqrt(2)-sqrt(6))/2
+
+    	## 6 right back   
+	elif key == 'x':
+	   back_vel = 5*sqrt(2)
+	   left_vel = -5*(sqrt(2)-sqrt(6))/2
+	   right_vel = -5*(sqrt(2)+sqrt(6))/2
+
+
     	## rotate       
 	elif key == 'h':
-	   vel0 = 10
-	   vel1 = 10
-	   vel2 = 10
+	   back_vel = 10
+	   left_vel = 10
+	   right_vel = 10
+
     	## stop    
 	elif key == 'j':
-	   vel0 = 0
-	   vel1 = 0
-	   vel2 = 0
+	   back_vel = 0
+	   left_vel = 0
+	   right_vel = 0
     	## exit   
 	else:
-	   vel0 = 0
-	   vel1 = 0
-	   vel2 = 0
+	   back_vel = 0
+	   left_vel = 0
+	   right_vel = 0
 	   exit()		
  
-	pub0.publish(vel0)
-	pub1.publish(vel1)
-	pub2.publish(vel2)
+	back_pub.publish(back_vel)
+	left_pub.publish(left_vel)
+	right_pub.publish(right_vel)
 	
 	rate.sleep()
-
